07f427d0b0eabb7c7211c5d75834d2ea2c524035
Radek 'blufor' Slavicinsky <radek@blufor.cz>
2017-01-14 01:07:20 +0100

    v1.6.2: better Slack actor message formatting

--- lib/notifu/actors/slack.rb
+++ lib/notifu/actors/slack.rb
@@ -15,7 +15,7 @@ module Notifu
           icon_emoji: ":loudspeaker:",
           attachments: [
             {
-              fallback: self.text,
+              fallback: self.text(self.fallback_template),
               color: self.color,
               title: "#{self.issue.host} - #{self.issue.service}",
               title_link: "#{Notifu::CONFIG[:uchiwa_url]}/#/client/#{self.issue.datacenter}/#{self.issue.host}?check=#{self.issue.service}",
@@ -38,7 +38,7 @@ module Notifu
         return {
           username: "notifu",
           icon_emoji: ":loudspeaker:",
-          text: self.text
+          text: self.text(self.template)
         }
       end
 
@@ -55,24 +55,53 @@ module Notifu
         end
       end
 
+      def status_icon
+        return @status_icon if defined? @status_icon
+        case self.issue.code.to_i
+        when 0
+          @status_icon = ":white_check_mark:"
+        when 1
+          @status_icon = ":exclamation:"
+        when 2
+          @status_icon = ":bangbang:"
+        else
+          @status_icon = ":question: _#{self.issue.code.to_s}_"
+        end
+        @status_icon
+      end
+
+      def service_icon
+        return @service_icon if defined? @service_icon
+        @service_icon = ":computer:" if self.issue.service == "keepalive"
+        @service_icon ||= ":gear:"
+      end
+
       # fallback simple message (templated, see below)
-      def text
-        data = OpenStruct.new({
+      def text(tmpl)
+        @tmpl_struct ||= OpenStruct.new({
           notifu_id: self.issue.notifu_id,
           datacenter: self.issue.datacenter,
           host: self.issue.host,
+          address: self.issue.address,
           service: self.issue.service,
           message: self.issue.message,
           status: self.issue.code.to_state,
           duration: (Time.now.to_i - self.issue.time_created.to_i).duration,
-          uchiwa_url: "#{Notifu::CONFIG[:uchiwa_url]}/#/client/#{self.issue.datacenter}/#{self.issue.host}?check=#{self.issue.service}"
+          uchiwa_url: "#{Notifu::CONFIG[:uchiwa_url]}/#/client/#{self.issue.datacenter}/#{self.issue.host}?check=#{self.issue.service}",
+          service_icon: self.service_icon,
+          status_icon: self.status_icon
         })
-        ERB.new(self.template).result(data.instance_eval {binding})
+        ERB.new(tmpl).result(@tmpl_struct.instance_eval {binding})
       end
 
       # template for fallback message
+      def fallback_template
+        "*<%= data[:status] %>* <%= data[:datacenter]%> <%= data[:host] %> <%= data[:service] %> - <%= data[:message] %> (<%= data[:duration] %>) <%= data[:uchiwa_url] %>"
+      end
+
+      # template for plain message
       def template
-        "**<%= data[:status] %>** [<<%= data[:uchiwa_url] %>|<%= data[:datacenter]%>/<%= data[:host] %>/<%= data[:service] %>>]: <%= data[:message] %> (<%= data[:duration] %>)"
+        "<%= data[:status_icon] %> *<<%= data[:uchiwa_url] %>|<%= data[:datacenter] %>/<%= data[:host] %>>* <%= data[:address] %> <%= data[:service_icon] %> *<%= data[:service] %>* - <%= data[:message] %> _<%= data[:duration] %>_"
       end
 
       def act
--- notifu.gemspec
+++ notifu.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name                  = 'notifu'
-  s.version               = '1.6.1'
+  s.version               = '1.6.2'
   s.date                  = Time.now.strftime('%Y-%m-%d')
   s.summary               = "Sensu notification handling service"
   s.description           = "Provides Nagios-like notification configuration & schedules"
